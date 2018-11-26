2059d5cbd67ef84728e98a6cc7ee2f94ac9cb3d7
Charlie Russo <cparusso@gmail.com>
2018-11-26 08:27:42 -0500

    Added the bare bones of the highlighted event section.

---
+++ src/components/HighlightedEvent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './styles/highlighted-event.css'
+
+class HighlightedEvent extends Component {
+  render() {
+    return (
+      <div className='highlight-section'>
+      </div>
+    )
+  }
+}
+
+export default HighlightedEvent
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react'
-import UpcomingEvents from './UpcomingEvents'
 import EventMap from './EventMap'
+import UpcomingEvents from './UpcomingEvents'
+import HighlightedEvent from './HighlightedEvent'
 import './styles/search.css'
 
 class Search extends Component {
   render() {
+    // will need to send the selected event's info down to the HighlightedEvent - it should initially load the next event that is happening in the area
+
     return (
       <div>
         <div className='event-section'>
           <EventMap />
           <UpcomingEvents />
         </div>
+        <HighlightedEvent />
       </div>
-    );
+    )
   }
 }
 
-export default Search;
+export default Search
