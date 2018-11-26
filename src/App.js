6cf06314e6d1e34dbcb66c77ad51e67d835047b5
Charlie Russo <cparusso@gmail.com>
2018-11-26 10:59:28 -0500

    new files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Search from './components/Search'
+import User from './components/User'
+import About from './components/About'
+import Error from './components/Error'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/user' component={User} />
+          <Route path='/search' component={Search} />
+          <Route path='/about' component={About} />
+          <Route component={Error} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
